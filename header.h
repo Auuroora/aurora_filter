6c7abaae486e0b2dea1dca75d2628e7df15ffffc
doroddong <52445743+doroddong@users.noreply.github.com>
2020-05-04 22:32:44 +0900

    Initialize Integrated Code

---
+++ header.h
@@ -0,0 +1,215 @@
+#pragma once
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif // _WIN32
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/core/ocl.hpp>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+// 색 공간 인덱스
+typedef enum {
+	B = 0, G, R,
+	H = 0, S, V
+} ColorSpaceIndex;
+
+// 작업중인 모든 변수 다 여기에
+class WorkingImgInfo {
+public:
+	WorkingImgInfo() {
+		// split할 벡터 메모리 할당
+		this->filter.bgr_filters.resize(3);
+		this->filter.bgr_filters.resize(3);
+	};
+
+	double min_h, max_h;
+	double min_s, max_s;
+	double min_v, max_v;
+	double min_r, max_r;
+	double min_g, max_g;
+	double min_b, max_b;
+
+	int row; // 다운사이징 후 사진 가로
+	int col; // 다운사이징 후 사진 세로
+
+	UMat downsizedImg;		// 다운사이징 후 이미지
+	UMat bgrImg, hsvImg;	// bgr이미지, hsv이미지
+	UMat resImg;			// 최종 결과물
+
+	// filter
+	struct Filter {
+		UMat diff;					// 필터 연산을 위한 행렬
+		UMat bgr_filter;			// bgr변경치가 기록되어 있는 필터
+		UMat hsv_filter;			// hsv변경치가 기록되어 있는 필터
+		
+		UMat clarity_filter;
+		UMat clarity_mask;
+
+		UMat gaussian_kernel;
+
+		UMat grain_mask;
+		UMat salt_mask;
+		UMat pepper_mask;
+
+		UMat exposure_mask;
+
+		vector<UMat> bgr_filters;	// split한 벡터(bgr)
+		vector<UMat> hsv_filters;	// split한 벡터(hsv)
+	} filter;
+
+	// 색 검출용 가중치 행렬
+	struct Weight {
+		UMat blue, green, red;
+		UMat hue, sat, val;
+	} weight;
+
+	// trackbar pos
+	// 현재 트랙바 상태 저장한 변수들
+	struct Trackbar {
+		//struct HSV {
+		//	struct Hue {
+		//		int	red = 0, orange = 0, yellow = 0, 
+		//			green = 0, blue = 0, violet = 0;
+		//	} hue;
+
+		//	struct Sat {
+		//		int	red = 0, orange = 0, yellow = 0,
+		//			green = 0, blue = 0, violet = 0;
+		//	} sat;
+
+		//	struct Val {
+		//		int	red = 0, orange = 0, yellow = 0,
+		//			green = 0, blue = 0, violet = 0;
+		//	} val;
+		//} hsv;
+
+		int temperature = 0;
+		int hue;
+		int saturation = 0;
+		int value = 0;
+		int vibrance = 0;
+		int highlight = 0;
+		
+		int tint = 0;
+		int clarity = 0;
+		int exposure = 0;
+		int gamma = 0;
+		int grain = 0;
+		int vignette = 0;
+
+	} trackbar;
+
+	// getter & setter
+	Mat getOriginImg() {
+		return this->originImg;
+	}
+
+	void setOriginImg(Mat img) {
+		this->originImg = img.clone();
+	}
+
+	UMat getResImg() {
+		return this->resImg;
+	}
+
+private:
+	Mat originImg; // 변경 불가한 원본 이미지(다운사이징 전)
+};
+
+class ParallelModulo : public ParallelLoopBody {
+private:
+	Mat &src;
+	Mat &dst;
+	short* dataSrc;
+	short* dataDst;
+	int mod;
+
+public:
+	ParallelModulo(Mat &src, Mat &dst, int mod) : src(src), dst(dst), mod(mod) {
+		dataSrc = (short*)src.data;
+		dataDst = (short*)dst.data;
+	}
+
+	virtual void operator ()(const Range& range) const CV_OVERRIDE {
+		for (int r = range.start; r < range.end; r++) {
+			dataDst[r] = (dataSrc[r] < 0 ? dataSrc[r] + mod : dataSrc[r] % mod);
+		}
+	}
+
+	ParallelModulo& operator=(const ParallelModulo &) {
+		return *this;
+	};
+};
+
+class ParallelMakeWeight : public ParallelLoopBody {
+private:
+	Mat &origin;
+	Mat &weighMatrix;
+	double min, max;
+	double(*weightFunc)(int, int);
+
+public:
+	ParallelMakeWeight(Mat &i, Mat &w, double(*wF)(int, int)) : origin(i), weighMatrix(w), weightFunc(wF) {
+		cv::minMaxIdx(origin, &min, &max);
+	}
+
+	virtual void operator ()(const Range& range) const CV_OVERRIDE {
+		for (int r = range.start; r < range.end; r++) {
+			weighMatrix.data[r] = 10.0;//weightFunc((int)origin.data[r], max);
+		}
+	}
+
+	ParallelMakeWeight& operator=(const ParallelMakeWeight &) {
+		return *this;
+	};
+};
+
+// core.cpp
+double GND(double x, double w, double std, double mu);
+double weightPerColor(int color, int val);
+double weightPerSaturation(int val, int mu);
+double weightPerValue(int val, int mu);
+void updateHue(int pos);
+void updateSaturation(int pos);
+void updateValue(int pos);
+void updateTemperature(int pos);
+void updateVibrance();
+void updateHighlightSaturation();
+void updateHighlightHue();
+void applyFilter();
+
+// callback
+void mouseCallback(int event, int x, int y, int flags, void *userdata);
+void onChangeHue(int pos, void* ptr);
+void onChangeSaturation(int v, void* ptr);
+void onChangeValue(int v, void* ptr);
+void onChangeTemperature(int v, void* ptr);
+void onChangeVibrance(int v, void* ptr);
+void onChangeHighlight(int curPos, void* ptr);
+
+
+// 테스트용
+void onChangeColorFilter(int curPos, void* ptr);
+
+extern WorkingImgInfo imginfo;
+
+/*********************************************************************
+*	이하 동훈이 코드
+*********************************************************************/
+void update_brightness(int pos);
+void update_constrast(int brightnessValue, int constrastValue);
+void update_clarity(int pos);
+void upadate_exposure(int pos);
+void update_gamma(int pos);
+void update_grain(int pos);
+void update_vignette(int pos);
+void update_tint(int pos);
---
+++ main.cpp
@@ -0,0 +1,184 @@
+﻿#include "define.h"
+#include "header.h"
+
+using namespace cv;
+using namespace std;
+
+WorkingImgInfo imginfo;
+
+void init(Mat &img) {
+	// save original Image
+	imginfo.setOriginImg(img);
+
+	// downsizing
+	imginfo.downsizedImg = img.clone().getUMat(ACCESS_RW);
+	imginfo.row = imginfo.downsizedImg.rows;
+	imginfo.col = imginfo.downsizedImg.cols;
+	// TO DO
+
+	// convert to 3 channels(BGRA -> BGR)
+	if (imginfo.downsizedImg.channels() == 4) {
+		cv::cvtColor(imginfo.downsizedImg, imginfo.downsizedImg, COLOR_BGRA2BGR);
+	}
+
+	// setting img
+	//imginfo.bgrImg = imginfo.downsizedImg.clone();
+	imginfo.bgrImg = imginfo.downsizedImg.clone();
+	cv::cvtColor(imginfo.bgrImg, imginfo.hsvImg, COLOR_BGR2HSV);
+	cv::split(imginfo.bgrImg, imginfo.filter.bgr_filters);
+	cv::split(imginfo.hsvImg, imginfo.filter.hsv_filters);
+
+	//*******************************************************************************************************
+
+	//Clarity
+	cv::bilateralFilter(imginfo.bgrImg, imginfo.filter.clarity_filter, DISTANCE, SIGMA_COLOR, SIGMA_SPACE);
+	imginfo.filter.clarity_mask = UMat::zeros(imginfo.col, imginfo.row, CV_16SC3);
+
+	//Vignette
+	Mat kernel_x, kernel_y, kernel_res;
+	kernel_x = cv::getGaussianKernel(imginfo.row, 1000);
+	kernel_y = cv::getGaussianKernel(imginfo.col, 1000);
+	cv::transpose(kernel_x, kernel_x);
+	kernel_res = (kernel_y * kernel_x);
+	cv::normalize(kernel_res, kernel_res, 0, 1, NORM_MINMAX);
+	imginfo.filter.gaussian_kernel = kernel_res.getUMat(ACCESS_RW);
+
+	//Grain    
+	imginfo.filter.grain_mask = UMat::zeros(imginfo.col, imginfo.row, CV_8S);
+
+	cv::randu(imginfo.filter.grain_mask, Scalar(-20), Scalar(20));
+	imginfo.filter.salt_mask = UMat(imginfo.col, imginfo.row, CV_8U);
+	imginfo.filter.pepper_mask = UMat(imginfo.col, imginfo.row, CV_8U);
+
+	//Exposure
+	imginfo.filter.exposure_mask = UMat::ones(imginfo.col, imginfo.row, CV_8UC1);
+
+
+
+	//*******************************************************************************************************
+
+	// cal minmax
+	cv::minMaxIdx(imginfo.filter.bgr_filters[ColorSpaceIndex::B], &imginfo.min_b, &imginfo.max_b);
+	cv::minMaxIdx(imginfo.filter.bgr_filters[ColorSpaceIndex::G], &imginfo.min_g, &imginfo.max_g);
+	cv::minMaxIdx(imginfo.filter.bgr_filters[ColorSpaceIndex::R], &imginfo.min_r, &imginfo.max_r);
+
+	cv::minMaxIdx(imginfo.filter.hsv_filters[ColorSpaceIndex::H], &imginfo.min_h, &imginfo.max_h);
+	cv::minMaxIdx(imginfo.filter.hsv_filters[ColorSpaceIndex::S], &imginfo.min_s, &imginfo.max_s);
+	cv::minMaxIdx(imginfo.filter.hsv_filters[ColorSpaceIndex::V], &imginfo.min_v, &imginfo.max_v);
+
+	// init weight and diff matrix
+	imginfo.weight.hue = UMat::ones(imginfo.row, imginfo.col, CV_32F);
+	imginfo.weight.sat = UMat::ones(imginfo.row, imginfo.col, CV_32F);
+	imginfo.weight.val = UMat::ones(imginfo.row, imginfo.col, CV_32F);
+	
+	imginfo.filter.bgr_filters[ColorSpaceIndex::B] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+	imginfo.filter.bgr_filters[ColorSpaceIndex::G] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+	imginfo.filter.bgr_filters[ColorSpaceIndex::R] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+
+	imginfo.filter.hsv_filters[ColorSpaceIndex::H] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+	imginfo.filter.hsv_filters[ColorSpaceIndex::S] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+	imginfo.filter.hsv_filters[ColorSpaceIndex::V] = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+
+	imginfo.filter.diff = UMat::zeros(imginfo.row, imginfo.col, CV_16S);
+
+	// make weight matrix
+	// TO DO
+
+	// TO DO
+}
+
+int main() {
+	/*********************************************************************
+	*	OpenCL Test
+	*********************************************************************/
+	// OpenCL을 사용할 수 있는지 테스트 
+	if (!ocl::haveOpenCL()) {
+		cout << "에러 : OpenCL을 사용할 수 없는 시스템입니다." << endl;
+		return -1;
+	}
+
+	// 컨텍스트 생성
+	ocl::Context context;
+	if (!context.create(ocl::Device::TYPE_GPU)) {
+		cout << " 에러 : 컨텍스트를 생성할 수 없습니다." << endl;
+		return -1;
+	}
+
+	// GPU 장치 정보
+	cout << context.ndevices() << " GPU device (s) detected " << endl;
+	for (size_t i = 0; i < context.ndevices(); i++) {
+		ocl::Device device = context.device(i);
+		cout << " - Device " << i << " --- " << endl;
+		cout << " Name : " << device.name() << endl;
+		cout << " Availability : " << device.available() << endl;
+		cout << "Image Support : " << device.imageSupport() << endl;
+		cout << " OpenCL C version : " << device.OpenCL_C_Version() << endl;
+	}
+
+	ocl::Device(context.device(0));
+	ocl::setUseOpenCL(true);
+
+	/*********************************************************************
+	*	Init
+	*********************************************************************/
+	Mat inputImg = imread("3024x3024.jpg", IMREAD_COLOR);
+	if (inputImg.empty()) {
+		cout << "Image Open Failed" << endl;
+		return -1;
+	}
+
+	init(inputImg);
+
+	/*********************************************************************
+	*	Make Window
+	*********************************************************************/
+	namedWindow(TEST_WINDOW, WINDOW_NORMAL);
+	resizeWindow(TEST_WINDOW, 400, 600);
+	//setMouseCallback(TEST_WINDOW, mouseCallback, &imginfo.resImg);
+
+	namedWindow(SET_WINDOW, WINDOW_NORMAL);
+	resizeWindow(SET_WINDOW, 500, 200);
+
+	/*********************************************************************
+	*	Make Trackbar
+	*********************************************************************/
+	//// HUE
+	//createTrackbar("Hue", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeHue);
+	//setTrackbarPos("Hue", SET_WINDOW, TRACKBAR_MID);
+
+	//// Saturation
+	//createTrackbar("Sat", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeSaturation);
+	//setTrackbarPos("Sat", SET_WINDOW, TRACKBAR_MID);
+
+	//// Value
+	//createTrackbar("Value", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeValue);
+	//setTrackbarPos("Value", SET_WINDOW, TRACKBAR_MID);
+
+	// Temperature
+	createTrackbar("Temperature", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeTemperature);
+	setTrackbarPos("Temperature", SET_WINDOW, TRACKBAR_MID);
+
+	//// Vibrance
+	//createTrackbar("Vibrance", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeVibrance);
+	//setTrackbarPos("Vibrance", SET_WINDOW, TRACKBAR_MID);
+
+	//// ColorFilter
+	//createTrackbar("R", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeColorFilter);
+	//setTrackbarPos("R", SET_WINDOW, TRACKBAR_MIN);
+	//createTrackbar("G", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeColorFilter);
+	//setTrackbarPos("G", SET_WINDOW, TRACKBAR_MIN);
+	//createTrackbar("B", SET_WINDOW, TRACKBAR_MIN, TRACKBAR_MAX, onChangeColorFilter);
+	//setTrackbarPos("B", SET_WINDOW, TRACKBAR_MIN);
+
+	//// Highlight
+	//createTrackbar("H", SET_WINDOW, TRACKBAR_MIN, HUE_MAX, onChangeHighlight);
+	//setTrackbarPos("H", SET_WINDOW, TRACKBAR_MIN);
+	//createTrackbar("S", SET_WINDOW, TRACKBAR_MIN, SAT_MAX, onChangeHighlight);
+	//setTrackbarPos("S", SET_WINDOW, TRACKBAR_MIN);
+
+	while (waitKey(0) != 27);
+	destroyAllWindows();
+	return 0;
+}
+
+
