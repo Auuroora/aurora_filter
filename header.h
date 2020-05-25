c24e48fe64dbb25169d2fd453fcea2f5a4119ec6
seong soo kim <ssoo2024@ajou.ac.kr>
2020-05-25 19:01:59 +0900

    filter preview fix

--- header.h
+++ header.h
@@ -31,56 +31,22 @@ void downsize_image(cv::Mat &src, cv::Mat &dst, int downsizedRow, int downsizedC
 void mouse_callback(int event, int x, int y, int flags, void *userdata);
 double weight_per_saturation(int val, int mu);
 double weight_per_value(int val, int mu);
-void apply_filter();
 cv::Mat get_preview_image(cv::Mat& img, cv::Mat logo);
 
-// change hls
-void update_hue(int pos);
 void on_change_hue(int pos, void* ptr);
-
-void update_saturation(int pos);
 void on_change_saturation(int v, void* ptr);
-
-void update_lightness(int pos);
 void on_change_lightness(int v, void* ptr);
-
-void update_vibrance(int pos);
 void on_change_vibrance(int v, void* ptr);
-
-void update_highlight_saturation(int pos);
 void on_change_highlight_saturation(int curPos, void* ptr);
-
-void update_highlight_hue(int pos);
 void on_change_highlight_hue(int curPos, void* ptr);
-
-void update_shadow_hue(int pos);
 void on_change_shadow_hue(int curPos, void* ptr);
-
-void update_shadow_saturation(int pos);
 void on_change_shadow_saturation(int curPos, void* ptr);
-
-// change bgr
-void update_temperature(int pos);
 void on_change_temperature(int v, void* ptr);
-
-void update_brightness_and_constrast(int brightness_pos, int constrast_pos);
 void on_change_bright(int pos, void *ptr);
-
-void update_tint(int pos);
 void on_change_tint(int pos, void *ptr);
-
 void on_change_constrast(int pos, void *ptr);
-
-void update_clarity(int pos);
 void on_change_clarity(int pos, void *ptr);
-
-void update_exposure(int pos);
-void update_gamma(int pos);
-void update_grain(int pos);
-void update_vignette(int pos);
-
 void on_change_bright(int pos, void *ptr);
-
 void on_change_exposure(int pos, void *ptr);
 void on_change_gamma(int pos, void *ptr);
 void on_change_grain(int pos, void *ptr);
@@ -89,17 +55,23 @@ void on_change_vignette(int pos, void *ptr);
 // 작업중인 모든 변수 다 여기에
 class WorkingImgInfo {
 public:
+	WorkingImgInfo() {
+		std::cout << "WorkingImgInfo Created!\n";
+	}
+
 	/*********************************************************************
 	*	variable and struct
 	*********************************************************************/
-	int row;									// 다운사이징 후 사진 가로
-	int col;									// 다운사이징 후 사진 세로
-	int changed_color_space;
+	int row;									// 다운사이징 후 사진 세로(row가 세로 맞음)
+	int col;									// 다운사이징 후 사진 가로
+	int preview_row;							// 필터 미리보기용 세로 사이즈
+	int preview_col;							// 필터 미리보기용 가로 사이즈
 
 	struct Image {
 		cv::Mat downsized;						// 다운사이징 후 이미지
 		cv::Mat bgr, hls, hsv, res;				// bgr이미지, hsv이미지, 최종 결과물
 		cv::Mat logo;
+		cv::Mat preview;
 		std::vector<cv::Mat> bgr_origins;		// split한 벡터(bgr)
 		std::vector<cv::Mat> hls_origins;		// split한 벡터(hls)
 		std::vector<cv::Mat> hsv_origins;		// split한 벡터(hsv)
@@ -162,6 +134,24 @@ public:
 	/*********************************************************************
 	*	method
 	*********************************************************************/
+	void update_hue(int pos);
+	void update_saturation(int pos);
+	void update_lightness(int pos);
+	void update_vibrance(int pos);
+	void update_highlight_saturation(int pos);
+	void update_highlight_hue(int pos);
+	void update_shadow_hue(int pos);
+	void update_shadow_saturation(int pos);
+	void update_temperature(int pos);
+	void update_brightness_and_constrast(int brightness_pos, int constrast_pos);
+	void update_tint(int pos);
+	void update_clarity(int pos);
+	void update_exposure(int pos);
+	void update_gamma(int pos);
+	void update_grain(int pos);
+	void update_vignette(int pos);
+	void apply_filter();
+
 	/* first initialize */
 	void init_all(cv::Mat& img, int downsized_row, int downsized_col) {
 		this->originImg = img.clone();
@@ -186,8 +176,13 @@ public:
 		cv::cvtColor(this->image.bgr, this->image.hls, cv::COLOR_BGR2HLS);
 		cv::cvtColor(this->image.bgr, this->image.hsv, cv::COLOR_BGR2HSV);
 
-		this->image.logo = cv::imread("./img/aurora_wartermark.png", cv::IMREAD_COLOR);
-		cv::resize(this->image.logo, this->image.logo, cv::Size(this->col, this->row), 0, 0, cv::INTER_AREA);
+		/* load logo(for watermark) */
+		this->set_logo_image(cv::imread("./img/aurora_wartermark.png", cv::IMREAD_COLOR));
+
+		/* filter preview image */
+		preview_row = 100;
+		preview_col = 100;
+		cv::resize(this->originImg, this->image.preview, cv::Size(preview_col, preview_row), 0, 0, cv::INTER_AREA);
 	}
 
 	/* filter matrix initialize */
@@ -278,8 +273,6 @@ public:
 		this->trackbar.vignette = pos;
 		this->trackbar.constrast = pos;
 		this->trackbar.brightness = pos;
-
-		this->changed_color_space = 0;
 	}
 
 	cv::Mat get_origin_img() {
@@ -288,6 +281,54 @@ public:
 
 	cv::Mat get_res_img() {
 		return this->image.res;
+	}
+	
+	void set_logo_image(cv::Mat logo) {
+		this->image.logo = logo;
+		cv::resize(this->image.logo, this->image.logo, cv::Size(this->col, this->row), 0, 0, cv::INTER_AREA);
+	}
+	//// 미리보기용
+	//cv::Mat get_filtered_image(int a = 0) {
+	//	
+	//}
+
+	cv::Mat get_filtered_image (
+		int hue = 0, int saturation = 0, int lightness = 0,int vibrance = 0,
+		int highlight_hue = 0, int highlight_sat = 0, int shadow_hue = 0, int shadow_sat = 0,
+		int temperature = 0, int tint = 0, int brightness = 0, int grain = 0,
+		int clarity = 0, int exposure = 0, int gamma = 0, int vignette = 0, int constrast = 0
+	) {
+		WorkingImgInfo preview_info;
+		preview_info.init_all(this->originImg, 200, 200);
+		
+		preview_info.update_hue(hue);
+		preview_info.update_saturation(saturation);
+		preview_info.update_lightness(lightness);
+		preview_info.update_vibrance(vibrance); 
+
+		preview_info.update_highlight_hue(highlight_hue);
+		preview_info.update_highlight_saturation(highlight_sat);
+		preview_info.update_shadow_hue(shadow_hue); 
+		preview_info.update_shadow_saturation(shadow_sat);
+
+		preview_info.update_temperature(temperature);
+		preview_info.update_tint(tint);
+		//preview_info.update_brightness_and_constrast(brightness);
+		preview_info.update_grain(grain);
+
+		preview_info.update_clarity(clarity);
+		preview_info.update_exposure(exposure);
+		//preview_info.update_gamma(gamma); std::cout << "hiii\n";
+		preview_info.update_vignette(vignette);
+		//preview_info.update_brightness_and_constrast(constrast);
+
+		preview_info.apply_filter();
+		return preview_info.image.res;
+	}
+
+	// 원본 저장용
+	cv::Mat get_filter_image_origin(std::vector<int> trackbar_pos) {
+
 	}
 private:
 	cv::Mat originImg; // 변경 불가한 원본 이미지(다운사이징 전)
--- test.cpp
+++ test.cpp
@@ -26,74 +26,74 @@ void mouse_callback(int event, int x, int y, int flags, void *userdata) {
 *	Trackbar Callback Function
 *********************************************************************/
 void on_change_hue(int curPos, void* ptr) {
-	update_hue(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_hue(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_saturation(int curPos, void* ptr) {
-	update_saturation(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_saturation(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_lightness(int curPos, void* ptr) {
-	update_lightness(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_lightness(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_temperature(int curPos, void* ptr) {
-	update_temperature(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_temperature(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_vibrance(int curPos, void* ptr) {
-	update_vibrance(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_vibrance(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_highlight_hue(int curPos, void* ptr) {
-	update_highlight_hue(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_highlight_hue(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_highlight_saturation(int curPos, void* ptr) {
-	update_highlight_saturation(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_highlight_saturation(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_shadow_hue(int curPos, void* ptr) {
-	update_shadow_hue(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_shadow_hue(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_shadow_saturation(int curPos, void* ptr) {
-	update_shadow_saturation(curPos - TRACKBAR_MID);
-	apply_filter();
+	imginfo.update_shadow_saturation(curPos - TRACKBAR_MID);
+	imginfo.apply_filter();
 	cv::imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_tint(int cur_pos, void *ptr) {
-	update_tint(cur_pos - TINT_MID);
-	apply_filter();
+	imginfo.update_tint(cur_pos - TINT_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_grain(int cur_pos, void *ptr) {
-	update_grain(cur_pos - GRAIN_MID);
-	apply_filter();
+	imginfo.update_grain(cur_pos - GRAIN_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_clarity(int cur_pos, void *ptr) {
-	update_clarity(cur_pos - CLARITY_MID);
-	apply_filter();
+	imginfo.update_clarity(cur_pos - CLARITY_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
@@ -102,32 +102,32 @@ int constrast = 100;
 
 void on_change_bright(int cur_pos, void *ptr) {
 	bright = (cur_pos - BRIGHTNESS_MID);
-	update_brightness_and_constrast(bright, constrast);
-	apply_filter();
+	imginfo.update_brightness_and_constrast(bright, constrast);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_constrast(int cur_pos, void *ptr) {
 	constrast = (cur_pos - CONSTRAST_MID);
-	update_brightness_and_constrast(bright, constrast);
-	apply_filter();
+	imginfo.update_brightness_and_constrast(bright, constrast);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_exposure(int cur_pos, void *ptr) {
-	update_exposure(cur_pos - EXPOSURE_MID);
-	apply_filter();
+	imginfo.update_exposure(cur_pos - EXPOSURE_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_gamma(int cur_pos, void *ptr) {
-	update_gamma(cur_pos - GAMMA_MID);
-	apply_filter();
+	imginfo.update_gamma(cur_pos - GAMMA_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
 
 void on_change_vignette(int cur_pos, void *ptr) {
-	update_vignette(cur_pos - VIGNETTE_MID);
-	apply_filter();
+	imginfo.update_vignette(cur_pos - VIGNETTE_MID);
+	imginfo.apply_filter();
 	imshow(TEST_WINDOW, imginfo.image.res);
 }
